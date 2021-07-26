af27af52bb62b40b48bdd6c7fd73734e1055cfac
carlos-bsc <carlos-bsc@protonmail.com>
2021-07-26 22:07:57 +0200

    under maintenance

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <SettingFilled style={{ fontSize: 70, marginBottom: "20px" , color: '#58c978' }} />
       <Title level={3} style={{color: "#58c978"}}>We will be back shortly</Title>
       <p style={{color:"white", fontWeight:"bold"}}>While you're waiting, take a look at what we looked like just two months ago...</p>
-      <iframe style={{ marginTop: "20px", width: "1000px", height: "100vh"}} src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRMkNvVcEBImKU3p-fiQT9ThnN77Ec6pxdMsFSJYPDtwt98DdmhLaO2RUhrrM01DjrMhE1TmwOKmGAZ/pubhtml?widget=true&amp;headers=false"></iframe>
+      <iframe title="doc" style={{ marginTop: "20px", width: "1000px", height: "100vh"}} src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRMkNvVcEBImKU3p-fiQT9ThnN77Ec6pxdMsFSJYPDtwt98DdmhLaO2RUhrrM01DjrMhE1TmwOKmGAZ/pubhtml?widget=true&amp;headers=false"></iframe>
       </Content>
     </Layout>
     </div>
